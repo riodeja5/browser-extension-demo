e783a87c53909dc21058b3114e1a5d175b20ee11
riodeja5 <riodeja.5.shota@gmail.com>
2023-02-22 22:42:02 +0900

    feat: language options, add Mantine UI

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -5,7 +5,13 @@ const Popup = (): ReactElement => {
   document.body.style.height = '15rem';
   return (
     <div className="flex h-screen items-center justify-center">
-      <h1>Popup</h1>
+      <p>どの言語にほんやくしますか？</p>
+      <select name="target_lang">
+        <option value="EN">英語</option>
+        <option value="KO">韓国語</option>
+        <option value="ZH">中国語</option>
+        <option value="JA">日本語</option>
+      </select>
     </div>
   );
 };
